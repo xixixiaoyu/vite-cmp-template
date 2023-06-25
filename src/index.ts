58405a1b898352ef8e30d41b4a7c48089d5d221f
yunmu <1416900346@qq.com>
2023-06-25 17:54:42 +0800

    支持 sfc 组件

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 
-import SButton from './button'
+import SFCButton from './button/SFCButton.vue'
 
-createApp(SButton).mount('#app')
+createApp(SFCButton).mount('#app')
